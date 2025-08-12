27752ad9b6cd6b637cbf292b9a79ef4f155e13af
mhd shazeb <mshazeb@phoenix.tech>
2025-08-12 07:25:48 +0530

    Create React project using 1. Vite 2. Configured the tailwind framework for css

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='bg-amber-100 text-3xl text-center p-2'>welcome to Netflix-Gpt</h1>
+    </div>
+  )
+}
+
+export default App
